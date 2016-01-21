32bd241a0553c13f0b3ef07ae351f91379aa6715
Brandon Jackson <bwjackson@oakland.edu>
2016-01-21 16:15:18 -0500

    added logic to read binary number from console input and added skeleton function for conversion

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome to the Binary to Decimal calculator Program\n");
+        System.out.println("Enter binary number value: ");
+        Scanner scanner = new Scanner(System.in);
+        String buffer = scanner.nextLine();
+        System.out.println("You entered: " + buffer + "for conversion.\n");
+        System.out.println("Procesing the data ....\n");
+        System.out.println("The decimal conversion is: " + bin2Dec(buffer));
+    }
+
+    public int bin2Dec(String input) {
+        
     }
 }
